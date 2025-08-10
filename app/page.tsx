8a6996067694473acd4787b4d5c291c6dde75584
hasnaa <hasnaalotfy11@gmail.com>
2025-08-10 03:58:36 +0300

    Initial clean commit

---
+++ app/page.tsx
@@ -0,0 +1,19 @@
+import { HeroSection } from './components/HeroSection';
+import { SkillsSection } from './components/SkillsSection';
+import { ProjectsSection } from './components/ProjectsSection';
+import { WebCapabilitiesSection } from './components/WebCapabilitiesSection';
+import { ContactSection } from './components/ContactSection';
+import Expertise from './components/Expertins';
+
+export default function WebDeveloperPortfolio() {
+	return (
+		<main className="min-h-screen bg-[#0D1117] text-white">
+			<HeroSection />
+			<SkillsSection />
+			<ProjectsSection />
+			<WebCapabilitiesSection />
+			<Expertise/>
+			<ContactSection />
+		</main>
+	);
+}
